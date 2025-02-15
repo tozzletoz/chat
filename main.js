e075896f81da37421376f5521595365e77547320
tozzletoz <robot189mail@gmail.com>
2025-02-15 20:10:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ const send = document.getElementById("send")
 const message = document.getElementById("message")
 const username = document.getElementById("username")
 const messages = document.getElementById("messages")
+const container = document.getElementById("container")
 
 let usernameText = localStorage.getItem("username") || null
 username.value = usernameText
 send.addEventListener("click", () => {
-	if  (message.value.trim() != ""){
+	if  (message.value.trim() != "" && username.value.trim() != ""){
 		console.log(encode(message.value))
 		const newMessage = document.createElement("p")
 		newMessage.style.backgroundColor = "rgba(60, 255, 0, 0.4)"
 		newMessage.id = "chatItem"
 		newMessage.textContent = `You: ${message.value}`
 		messages.appendChild(newMessage)
+		container.scrollTop = messages.scrollHeight
 		socket.send(JSON.stringify({
 			"method": "set",
 			"user": "player",
@@ -43,6 +45,7 @@ socket.addEventListener("message", (resp) => {
 	newMessage.id = "chatItem"
 	newMessage.textContent = decode(JSON.parse(resp.data).value)
 	messages.appendChild(newMessage)
+	container.scrollTop = messages.scrollHeight
 })
 
 username.addEventListener("input", () => {
